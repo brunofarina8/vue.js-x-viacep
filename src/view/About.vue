fb29d4681bcc285a22fc1ea11e6b313e0c202305
Bruno Farina <brfarina2003@gmail.com>
2023-11-22 15:33:27 -0300

    First Commit

---
+++ src/view/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Sobre o App</h1>
+        <p>Desenvolvidor por Bruno Farina</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
